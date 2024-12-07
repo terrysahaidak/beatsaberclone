95b239e48d30f234a35efb4dc2d1bb44870c8b5f
Terry Sahaidak <tasssik@gmail.com>
2024-12-07 09:07:50 +0200

    fix block scale; add appearing animation

--- src/components/Block.tsx
+++ src/components/Block.tsx
@@ -2,7 +2,7 @@ import { useEffect, useRef } from 'react';
 import * as THREE from 'three';
 import { useFrame } from '@react-three/fiber';
 import { useSpring, animated } from '@react-spring/three';
-import { LEFT_HAND_COLOR, RIGHT_HAND_COLOR } from '../constants';
+import { BOX_SIZE, LEFT_HAND_COLOR, RIGHT_HAND_COLOR } from '../constants';
 import { BlockModel } from '../store/BlockModel';
 import { useObject } from '../hooks/useObject';
 import blockDirectional from '../assets/block-directional.obj?url';
@@ -11,7 +11,7 @@ import { Direction } from '../types';
 import { getPositionForBlock, getRotationForDirection } from '../utils';
 import { observer } from 'mobx-react';
 
-const SCALE_FACTOR = 0.4;
+const SCALE_FACTOR = 1;
 
 export const Block = observer(function Block({ model }: { model: BlockModel }) {
   const ref = useRef<THREE.Group>(null);
@@ -19,11 +19,18 @@ export const Block = observer(function Block({ model }: { model: BlockModel }) {
 
   const color = model.type === 1 ? RIGHT_HAND_COLOR : LEFT_HAND_COLOR;
 
-  const [springs, springsApi] = useSpring(() => ({
+  const { x, y, z } = getPositionForBlock(model);
+  const rotationZ = getRotationForDirection(model.cutDirection);
+
+  const [springs] = useSpring(() => ({
     from: {
+      y: 0,
+      rotationZ: 0,
       scale: model.initial ? SCALE_FACTOR : 0,
     },
     to: {
+      y,
+      rotationZ,
       scale: SCALE_FACTOR,
     },
     config: {
@@ -34,14 +41,10 @@ export const Block = observer(function Block({ model }: { model: BlockModel }) {
 
   const group = useObject(model.cutDirection === Direction.ANY ? blockCenter : blockDirectional);
 
-  const rotation: THREE.Vector3Tuple = [0, 0, getRotationForDirection(model.cutDirection)];
-
-  const { x, y, z } = getPositionForBlock(model);
-
   useEffect(() => {
     model.setOnCollisionCallback(() => {
       if (ref.current) {
-        springsApi.start({ scale: 0, config: { duration: 50 } });
+        ref.current.visible = false;
       }
     });
   }, []);
@@ -59,13 +62,18 @@ export const Block = observer(function Block({ model }: { model: BlockModel }) {
   const geometry = (group.children[0] as THREE.Mesh).geometry;
 
   return (
-    <animated.group ref={ref} scale={springs.scale} position={[x, y, z]} rotation={rotation}>
-      <mesh ref={meshRef} scale={[SCALE_FACTOR, SCALE_FACTOR, SCALE_FACTOR]}>
+    <animated.group ref={ref} scale={springs.scale} position={springs.y.to((v) => [x, v, z])} rotation-z={springs.rotationZ}>
+      {/* <mesh ref={meshRef}>
+        <planeGeometry args={[BOX_SIZE, BOX_SIZE]} />
+        <meshStandardMaterial color={color} />
+      </mesh> */}
+
+      <mesh ref={meshRef} scale={[0.25, 0.25, 0.5]}>
         <primitive object={geometry} attach="geometry" />
         <meshStandardMaterial transparent attach="material" metalness={0.5} roughness={0.4} color={color} />
       </mesh>
-      <mesh position={[0, 0, 0.15]}>
-        <planeGeometry attach="geometry" args={[0.5, 0.5]} />
+      <mesh scale={0.9}>
+        <planeGeometry attach="geometry" args={[BOX_SIZE, BOX_SIZE]} />
         <meshLambertMaterial attach="material" emissive={0xffffff} />
       </mesh>
     </animated.group>
