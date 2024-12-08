16bfe129f22871b177e0695f0d2ecb8efa79635d
Terry Sahaidak <tasssik@gmail.com>
2024-12-08 18:47:46 +0200

    even better offsets

--- src/constants.ts
+++ src/constants.ts
@@ -4,20 +4,20 @@ export const LEFT_HAND_COLOR = '#ff000c';
 export const RIGHT_HAND_COLOR = '#123bff';
 
 export const BOX_SIZE = 0.35;
-export const PLAYER_Y_OFFSET = 0.5;
+export const PLAYER_Y_OFFSET = 0.3;
 
 export const STAGE_WIDTH = 3;
 export const STAGE_LENGTH = 2;
 export const SPAWN_STAGE_WIDTH = 4.75;
 export const SPAWN_STAGE_LENGTH = 100;
 export const SPAWN_STAGE_POSITION_Z = 8;
-export const GRID_PADDING = 0.3;
+export const GRID_PADDING = 0.6;
 export const GRID_WIDTH = STAGE_WIDTH - GRID_PADDING * 2;
 export const GRID_CELL_SIZE = GRID_WIDTH / 4;
 export const GRID_HEIGHT = GRID_CELL_SIZE * 3;
 
 export const GRID_Y = GRID_PADDING;
-export const GRID_X = -GRID_WIDTH / 2 + GRID_PADDING;
+export const GRID_X = -GRID_WIDTH / 2 + GRID_PADDING / 2;
 
 export const SONG_OFFSET = 0;
 
